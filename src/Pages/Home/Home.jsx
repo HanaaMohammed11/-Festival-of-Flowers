f448e63e8c791e57986d9e9db925693995e49712
HanaaMohammed11 <hanaamohammed840@gmail.com>
2024-11-15 17:52:56 -0800

    edit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import NavBar from '../../Components/Nav/NavBar'
 import Header from '../../Components/Header/Header'
 import PlantsSlider from '../../Components/PlantSlider/PlantsSlider'
+import Reviews from '../../Components/Reviews/Reviews'
+import Footer from '../../Components/Footer/Footer'
 
 export default function Home() {
   return (
     <div>
     <Header/>
     <PlantsSlider/>
+    <Reviews/>
+    <Footer/>
 
     </div>
   )
