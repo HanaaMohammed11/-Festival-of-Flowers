6e4e9a3233eea46f13504f2a62e84c900d169723
HanaaMohammed11 <hanaamohammed840@gmail.com>
2024-11-29 20:32:24 -0800

    search input

--- src/Components/Tools.jsx/ToolsSideBar/ToolsSideBar.jsx
+++ src/Components/Tools.jsx/ToolsSideBar/ToolsSideBar.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import ToolCard from "../ToolCard/ToolCard";
+import { CiSearch } from "react-icons/ci";
 
 function ToolsSideBar() {
-  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   const handleCategoryChange = (event) => {
     setSelectedCategory(event.target.value);
@@ -10,11 +11,22 @@ function ToolsSideBar() {
 
   return (
     <div className="font-cairo ">
+      <div className="relative pt-5 w-[30%] ml-2 ">
+        <input
+          className="rounded-full w-52 h-7 pr-2  text-right border-2 border-green-900 "
+          placeholder="...بحث"
+        />
+        <span className="absolute left-5 top-9 transform -translate-y-1/2 text-gray-500">
+          <CiSearch />
+        </span>
+      </div>
       <div className="flex-col md:flex-row sm:flex xxs:flex-col xs:flex-col lg:flex-row  ">
         {/* Fixed Sidebar */}
         <div className="w-[20%]  z-10 lg:right-0 xs:right-20 xxs:right-32  md:right-0 p-5">
           <div>
-            <h1 className="font-semibold mb-5 text-right lg:text-xl md:text-xl sm:text-md text-md lg:block md:block sm:hidden hidden xl:block">التصنيفات</h1>
+            <h1 className="font-semibold mb-5 text-right lg:text-xl md:text-xl sm:text-md text-md lg:block md:block sm:hidden hidden xl:block">
+              التصنيفات
+            </h1>
 
             {/* Dropdown for small screens */}
             <div className="sm:block md:hidden xs:block xxs:block hidden right-1 lg:hidden ">
@@ -23,7 +35,9 @@ function ToolsSideBar() {
                 onChange={handleCategoryChange}
                 className=" p-1 bg-[#375744] text-right text-white rounded-lg focus:outline-none"
               >
-                <option value="" disabled>اختر التصنيف</option>
+                <option value="" disabled>
+                  اختر التصنيف
+                </option>
                 <option value="fertilizers">اسمدة</option>
                 <option value="pots">اصص</option>
                 <option value="protection-tools">أدوات الحماية</option>
@@ -38,37 +52,49 @@ function ToolsSideBar() {
               <li className="flex justify-between items-center bg-[#375744] dark:bg-gray-700 dark:hover:bg-gray-600 p-2 rounded-lg cursor-pointer transition-all duration-300">
                 <button className="flex justify-between w-full">
                   <span className="text-white dark:text-gray-300">20</span>
-                  <span className="text-white dark:text-white font-medium">اسمدة</span>
+                  <span className="text-white dark:text-white font-medium">
+                    اسمدة
+                  </span>
                 </button>
               </li>
               <li className="flex justify-between items-center bg-[#375744] dark:bg-gray-700 dark:hover:bg-gray-600 p-2 rounded-lg cursor-pointer transition-all duration-300">
                 <button className="flex justify-between w-full">
                   <span className="text-white dark:text-gray-300">60</span>
-                  <span className="text-white dark:text-white font-medium">اصص</span>
+                  <span className="text-white dark:text-white font-medium">
+                    اصص
+                  </span>
                 </button>
               </li>
               <li className="flex justify-between items-center bg-[#375744] dark:bg-gray-700 dark:hover:bg-gray-600 p-2 rounded-lg cursor-pointer transition-all duration-300">
                 <button className="flex justify-between w-full">
                   <span className="text-white dark:text-gray-300">70</span>
-                  <span className="text-white dark:text-white font-medium">أدوات الحماية</span>
+                  <span className="text-white dark:text-white font-medium">
+                    أدوات الحماية
+                  </span>
                 </button>
               </li>
               <li className="flex justify-between items-center bg-[#375744] dark:bg-gray-700 dark:hover:bg-gray-600 p-2 rounded-lg cursor-pointer transition-all duration-300">
                 <button className="flex justify-between w-full">
                   <span className="text-white dark:text-gray-300">10</span>
-                  <span className="text-white dark:text-white font-medium">أدوات الري</span>
+                  <span className="text-white dark:text-white font-medium">
+                    أدوات الري
+                  </span>
                 </button>
               </li>
               <li className="flex justify-between items-center bg-[#375744] dark:bg-gray-700 dark:hover:bg-gray-600 p-2 rounded-lg cursor-pointer transition-all duration-300">
                 <button className="flex justify-between w-full">
                   <span className="text-white dark:text-gray-300">40</span>
-                  <span className="text-white dark:text-white font-medium">أدوات الحفر</span>
+                  <span className="text-white dark:text-white font-medium">
+                    أدوات الحفر
+                  </span>
                 </button>
               </li>
               <li className="flex justify-between items-center bg-[#375744] dark:bg-gray-700 dark:hover:bg-gray-600 p-2 rounded-lg cursor-pointer transition-all duration-300">
                 <button className="flex justify-between w-full">
                   <span className="text-white dark:text-gray-300">40</span>
-                  <span className="text-white dark:text-white font-medium">أدوات التقليم</span>
+                  <span className="text-white dark:text-white font-medium">
+                    أدوات التقليم
+                  </span>
                 </button>
               </li>
             </ul>
@@ -76,11 +102,10 @@ function ToolsSideBar() {
         </div>
 
         {/* Main Content */}
-        <div className='lg:w-[80%] md:w-[75%] flex justify-center  items-center'>
+        <div className="lg:w-[80%] md:w-[75%] flex justify-center  items-center">
           <ToolCard />
         </div>
       </div>
-      
     </div>
   );
 }
